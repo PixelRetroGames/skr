dc0c5ca6567b8097591bb642843afef659be33be
RSiminica <rsiminica@yahoo.ro>
2019-10-24 11:02:20 +0300

    Sort

--- main.cpp
+++ main.cpp
@@ -10,11 +10,24 @@ int dif(int a, int b) {
 int impartit(int a, int b) {
 	return a/b;
 }
+
 int modulo(int n) {
 	if(a < 0) {
 		return a * (-1);
 	}
 	return a;
+}
+
+int sort_vect(int n, int *p) {
+	for(int i = 0; i < n; i++) {
+		for(int j = i+1; j < n; j++) {
+			if(p[i] < p[j]) {
+				int aux = p[i];
+				p[i] = p[j];
+				p[j] = aux;
+			}
+		}
+	}
 }
 
 int main()
